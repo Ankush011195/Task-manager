7746dbae5e3e9480a288e71290d344e3037b0842
Ankush011195 <mehraankush188@gmail.com>
2026-05-05 15:03:49 +0530

    fix port 8080

--- backend/server.js
+++ backend/server.js
@@ -26,5 +26,5 @@ app.get("/", (req, res) => {
  mongoose.connect(process.env.MONGO_URL)
  .then(()=> console.log('Connected to MongoDB'))
  .catch((err)=> console.log(err));
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
